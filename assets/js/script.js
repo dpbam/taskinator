9ce3683bda306914fe42e0b6b0dea143fbc4fb5b
Derek Phelps <derekphtpt@gmail.com>
2021-05-26 16:09:25 -0600

    4.2.5 form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
-// console.dir(window.document);
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 // console.log(tasksToDoEl);
 // console.log(buttonEl);
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
-
+formEl.addEventListener("submit", createTaskHandler);
 
 // this is also calling the function
 // createTaskHandler();
