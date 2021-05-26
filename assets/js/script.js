c5008feb47021c4c989cbeaca99e372ccf90e194
Derek Phelps <derekphtpt@gmail.com>
2021-05-26 13:26:35 -0600

    add ability to add task again

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@
 
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+// console.log(tasksToDoEl);
+console.log(buttonEl);
 
 var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-    };
+};
 
 buttonEl.addEventListener("click", createTaskHandler);
 
+
+// this is also calling the function
+// createTaskHandler();
